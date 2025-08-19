5c439dd3e48e990d1d0ffb7609e51194d4d1eb43
Arianna Feijoo <adfeijoo@espol.edu.ec>
2025-08-18 23:49:42 -0500

    AddAeropuertoFXML (sin terminar 3)

--- ProyectoEstructuras/src/aeropuertovuelos/FXMLDocumentController.java
+++ ProyectoEstructuras/src/aeropuertovuelos/FXMLDocumentController.java
@@ -4,12 +4,21 @@
  */
 package aeropuertovuelos;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -17,10 +26,24 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private MenuButton aeropuertoMenu;
+    @FXML
+    private MenuItem añadirAero;
+    @FXML
+    private MenuItem eliminarAero;
+    @FXML
+    private MenuButton vueloMenu;
+    @FXML
+    private MenuItem añadirVuelo;
+    @FXML
+    private MenuItem editarVuelo;
+    @FXML
+    private MenuItem eliminarVuelo;
+    @FXML
+    private Button buscarRutaButtom;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -30,5 +53,41 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+
+    @FXML
+    private void añadirVueloHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void editarVueloHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminarVueloHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void buscarRuta(ActionEvent event) {
+    }
+
+    @FXML
+    private void abrirAgregarAeropuertoHandler(ActionEvent event) {
+        try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AddAeropuertoFXML.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Agregar Aeropuerto");
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void abrirEliminarAeroHandler(ActionEvent event) {
+    }
     
 }
