495fc6052aabc2f638373035291daca477c43df7
sizaguir <spizagui@espol.edu.ec>
2025-08-22 21:27:46 -0500

    Atributos añadidos a la clase Vuelo

--- ProyectoEstructuras/src/aeropuertovuelos/Vuelo.java
+++ ProyectoEstructuras/src/aeropuertovuelos/Vuelo.java
@@ -1,10 +1,14 @@
 package aeropuertovuelos;
 
-public class Vuelo {
+import java.io.Serializable;
+
+public class Vuelo implements Serializable{
     private Aeropuerto origen;
     private Aeropuerto destino;
     private double peso; // Puede ser distancia en km, costo o tiempo en horas
     private String aerolinea;
+    private int horaSalida;  // en minutos desde 00:00 (ej: 480 = 8:00 am)
+    private int horaLlegada;
 
     public Vuelo(Aeropuerto origen, Aeropuerto destino, double peso) {
         this.origen = origen;
@@ -15,6 +19,15 @@ public class Vuelo {
     public Vuelo(Aeropuerto origen, Aeropuerto destino, double peso, String aerolinea) {
         this(origen, destino, peso);
         this.aerolinea = aerolinea;
+    }
+    
+     public Vuelo(Aeropuerto origen, Aeropuerto destino, double peso, String aerolinea, int horaSalida, int horaLlegada) {
+        this.origen = origen;
+        this.destino = destino;
+        this.peso = peso;
+        this.aerolinea = aerolinea;
+        this.horaSalida = horaSalida;
+        this.horaLlegada = horaLlegada;
     }
 
     // Getters y setters
@@ -45,9 +58,36 @@ public class Vuelo {
     public void setAerolinea(String aerolinea) { 
         this.aerolinea = aerolinea; 
     }
+    
+    public int getHoraSalida() { 
+        return horaSalida; 
+    }
+    
+    public int getHoraLlegada() { 
+        return horaLlegada; 
+    }
+    
+    public void setHoraSalida(int horaSalida) { 
+        this.horaSalida = horaSalida; 
+    }
+    
+    public void setHoraLlegada(int horaLlegada) { 
+        this.horaLlegada = horaLlegada; 
+    }
 
+//    @Override
+//    public String toString() {
+//        return origen.getCodigo() + " → " + destino.getCodigo() + " | " + peso + " | " + aerolinea;
+//    }
+    
     @Override
     public String toString() {
-        return origen.getCodigo() + " → " + destino.getCodigo() + " | " + peso + " | " + aerolinea;
+        return origen.getCodigo() + " -> " + destino.getCodigo() + " | " + aerolinea + " | Salida: " + formatearHora(horaSalida) + " | Llegada: " + formatearHora(horaLlegada);
+    }
+
+    private String formatearHora(int minutos) {
+        int h = minutos / 60;
+        int m = minutos % 60;
+        return String.format("%02d:%02d", h, m);
     }
 }
